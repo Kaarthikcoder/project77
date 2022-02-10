f9f480c1b9df8a1a5dbb6d845947c3ce2b8de994
Kaarthikcoder <91957912+Kaarthikcoder@users.noreply.github.com>
2022-02-10 17:03:26 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+console.log("My first Console Project")
+
+var first_name=("My name is Kaartik")
+console.log(first_name);
+
+var a=200;
+var b=50;
+
+var add  = a + b;
+console.log(add);
+
+var sub = a - b;
+console.log(sub);
+
+var multiple = a * b;
+console.log(multiple);
+
+var divide = a / b;
+console.log(divide);
